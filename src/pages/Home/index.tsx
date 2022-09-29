1e2459ae8559dded6a85bdb4f565e2c5e1e330f7
GeronimoNeto <102226234+GeronimoNeto@users.noreply.github.com>
2022-09-29 15:00:58 -0300

    db server

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,19 @@
-import "./styles.css"
+import { useNavigate } from "react-router-dom"
+
 import {Button} from "../../components/Button"
 
 export function Home(){
+    const  navigate = useNavigate()
+
+    function nevegateToRegister(){
+        navigate('cadastrar-pedidos')
+    }
+
     return(
         <main>
             <section className="page-control">
                 <h2 className="title">Pedidos Pendentes</h2>
-                <Button>Cadastrar pedido</Button>
+                <Button onClick={nevegateToRegister}>Cadastrar pedido</Button>
             </section>
         </main>
     )
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,8 +1,58 @@
+import {FormEvent, useState} from "react"
+import {api} from "../../services/api"
+import "./styles.css"
+import { Button } from "../../components/Button"
+
 export function Register(){
+
+    const [description, setDescription] = useState("")
+    const [price, setPrice] = useState("")
+    const [optional, setOptional] = useState("")
+    
+    async function registerOrder(event:FormEvent){
+        event.preventDefault()
+
+        const response = await api.post('/orders',{
+            description,price,optional
+        })
+
+        console.table(response)
+    }
+
     return(
         <main>
             <section>
                 <h1>Pedidos Cadastrados</h1>
+                <form className="formMain" onSubmit={registerOrder}>
+                <div className="formControl">
+                    <label htmlFor="description">Descrição</label>
+                    <input
+                        placeholder="Descreva o pedido"
+                        onChange={(event)=>{setDescription(event.target.value)}} 
+                        value={description} 
+                    />
+                </div>
+                <div className="formControl">
+                    <label htmlFor="price">Preço</label>
+                    <input
+                        placeholder="Defina um preço"
+                        onChange={(event)=>{setPrice(event.target.value)}} 
+                        value={price} 
+                    />
+                </div>
+                <div className="formControl">
+                    <label htmlFor="opcional">Observações (opicional) </label>
+                    <input
+                        placeholder="Observações sobre o pedido"
+                        onChange={(event)=>{setOptional(event.target.value)}} 
+                        value={optional} 
+                    />
+                </div>
+                
+                <Button>
+                    Botão
+                </Button>
+            </form>
             </section>
         </main>
     )
